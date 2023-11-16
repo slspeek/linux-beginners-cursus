a3e99898b64151ca6d45626e1b36385fbc8ca6f5
Steven L. Speek <slspeek@gmail.com>
2023-11-16 15:08:32 +0100

    verwijdering google-chrome, discord voorwaardelijk

--- bin/prepare-education-box.sh
+++ bin/prepare-education-box.sh
@@ -11,4 +11,6 @@ wget https://github.com/slspeek/linux-beginners-cursus/releases/latest/download/
 cd ~/Afbeeldingen
 unzip ~/Downloads/bloemen.zip
 
-sudo apt-get -y remove chromium google-chrome-stable discord
+sudo apt-get -y remove chromium
+if which google-chrome; then sudo apt-get remove -y google-chrome-stable; fi
+if which  discord; then sudo apt-get remove -y discord; fi
